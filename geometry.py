06a144a844b0da7e0272f409f62f6114713d92f5
Louis Bouchard <louis.bouchard@ubuntu.com>
2014-10-07 15:02:58 +0200

    Version initiale
    Signed-off-by: Louis Bouchard <louis.bouchard@ubuntu.com>

---
+++ geometry.py
@@ -0,0 +1,25 @@
+class Point(object):
+
+	def __init__(self, x, y):
+		pass
+
+	def distance(self, otherpoint):
+		pass
+
+class Figure(object):
+
+	def __init(self):
+		pass
+
+	def position(self):
+		pass
+
+	def perimetre(self):
+		pass
+
+class Cercle(Figure):
+	pass
+
+class Carre(Figure):
+	pass
+
