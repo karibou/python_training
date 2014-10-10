362c2ee07d3d26871903d162bb03290eca425650
Louis Bouchard <louis.bouchard@ubuntu.com>
2014-10-10 16:13:59 +0200

    .
    Signed-off-by: Louis Bouchard <louis.bouchard@ubuntu.com>

---
+++ url.py
@@ -0,0 +1,46 @@
+from urllib2 import urlopen
+import re
+import sys
+
+#    stream = urlopen('http://x-server.gmca.aps.anl.gov/cgi/WWW_form.exe')
+#    html = stream.read()
+#    stream.close()
+
+def create_url(baseurl, param_dict):
+    option_list='?'
+    for key, param in param_dict.iteritems():
+        option_list=option_list + '{}={}'.format(key,param) + '&'
+    option_list = option_list.rstrip("&")
+    return baseurl + option_list
+
+def fetch_data(fullurl):
+    try:
+        stream = urlopen(fullurl)
+        html = stream.read()
+        stream.close
+        return html
+    except:
+        print "Erreur"
+
+def parse_result(html_data,regexp):
+    line = re.search(regexp,html_data)
+    if line:
+        print line.string
+
+if __name__ == '__main__':
+
+    URL='http://x-server.gmca.aps.anl.gov/cgi/X0h_form.exe'
+    Dict={ 'xway': 2,
+                'wave': 12.4,
+                'coway': 0,
+                'code': 'Silicon',
+                'i1': 1,
+                'i2': 1,
+                'i3': 1,
+                'df1df2':-1,
+                'modeout':1
+                }
+
+    print create_url(URL, Dict)
+    Data = fetch_data(create_url(URL, Dict))
+    parse_result(Data,sys.argv[1])
